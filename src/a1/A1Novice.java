d187e1ca1747359b1ef5cfcc2f161f35d938dd43
zeque <zeque@zeque-pc.wireless-1x.unc.edu>
2019-09-02 01:07:14 -0400

    Completed A1Novice assignment.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,39 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		// Read in the customer count
+		int customerCount = scan.nextInt();
+		
+		// Create arrays to hold the first names, last names, and total purchased
+		// of the customers.
+		String[] customersFirstNames = new String[customerCount];
+		String[] customersLastNames = new String[customerCount];
+		double[] customersTotalPurchased = new double[customerCount];
+		
+		for(int i = 0;i < customerCount;i++) {
+			// Read in information for each customer.
+			customersFirstNames[i] = scan.next();
+			customersLastNames[i] = scan.next();
+			
+			// Go through each item each customer has purchased
+			// and add the cost of those items to the total sum
+			// purchased by the customer.
+			int itemsBought = scan.nextInt();
+			
+			for(int j = 0;j < itemsBought;j++) {
+				int itemQuantity = scan.nextInt();
+				String itemName = scan.next(); // This is read purely to avoid scanner order errors.
+				double itemPrice = scan.nextDouble();
+				
+				customersTotalPurchased[i] += itemQuantity * itemPrice;
+			}
+		}
+		
+		// Print out information gathered from each customer.
+		for(int i = 0;i < customerCount;i++) {
+			System.out.println(customersFirstNames[i].charAt(0) + ". " +
+					customersLastNames[i] + ": " +
+					String.format("%.2f",customersTotalPurchased[i]));
+		}
 	}
 }
